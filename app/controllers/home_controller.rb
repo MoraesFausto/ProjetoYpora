afddfcdf7cc9aeb5ffaf0d40e4d64cc49edc8f1b
f4ustx <faustoalexandre860@gmail.com>
2021-09-17 18:17:22 -0300

    Ypora-0.0.5

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,8 @@
 class HomeController < ApplicationController
   def index
     @name = user_signed_in? ? current_user.email : 'Visitante'
-    @country = request.location.country
-    @city = request.location.city
   end
+  
   def about
   end
   def irrig
